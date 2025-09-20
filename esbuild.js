cdc6c80b4751d31858929297cec45294490e6214
Phyo Wai Lin <phyowailin2004@proton.me>
2025-09-21 06:28:16 +0630

    build: update esbuild script to output ESM, IIFE, and CJS formats
    Updated the build script to generate bundles in multiple
    formats (ESM, IIFE, CJS) for broader compatibility.

--- esbuild.js
+++ esbuild.js
@@ -1,32 +1,47 @@
 import process from 'process';
 import esbuild from 'esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
+import { format } from 'path/win32';
 
 const isProduction = process.env['ENV'] == 'production';
 
-/** @type {import('esbuild').BuildOptions} */
-const options = {
-  entryPoints: ['src/main.ts', 'src/main.scss'],
-  outdir: isProduction ? 'dist' : 'docs/assets',
-  bundle: true,
-  logLevel: 'info',
-  minify: isProduction,
-  sourcemap: !isProduction,
-  sourcesContent: !isProduction,
-  plugins: [
-    sassPlugin({
-      filter: /\.(s[ac]ss|css)$/,
-      type: 'css',
-      style: isProduction ? 'compressed' : 'expanded',
-      sourceMap: !isProduction,
-      sourceMapIncludeSources: !isProduction,
-    }),
-  ],
-};
+/** @type {import('esbuild').Format} */
+const formats = ['esm', 'iife', 'cjs'];
+
+/**
+ *
+ * @param {import('esbuild').Format} format
+ * @return {import('esbuild').BuildOptions}
+ */
+function getBuildOptions(format) {
+  return {
+    entryPoints: ['src/main.ts', 'src/main.scss'],
+    outdir: `dist/${format}`,
+    format,
+    bundle: true,
+    logLevel: 'info',
+    minify: false,
+    sourcemap: !isProduction,
+    sourcesContent: !isProduction,
+    plugins: [
+      sassPlugin({
+        filter: /\.(s[ac]ss|css)$/,
+        type: 'css',
+        style: isProduction ? 'compressed' : 'expanded',
+        sourceMap: !isProduction,
+        sourceMapIncludeSources: !isProduction,
+      }),
+    ],
+  };
+}
 
 if (isProduction) {
-  await esbuild.build(options);
+  Promise.all(
+    formats.map((format) => {
+      return esbuild.build(getBuildOptions(format));
+    }),
+  );
 } else {
-  const ctx = await esbuild.context(options);
+  const ctx = await esbuild.context(getBuildOptions('iife'));
   await ctx.watch();
 }
